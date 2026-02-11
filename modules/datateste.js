e0b35c9d270d9e6d25bcbaa7aeb6a1f7fb5f9484
Manuel Jose Medeiros Esteves <mjme.iefp@alunos.ipb.pt>
2026-02-11 12:08:30 +0000

    criar dados para testes

---
+++ modules/datateste.js
@@ -0,0 +1,4 @@
+export const transacoes = [
+    {transacao: "el Frances", categoria: "Despesa", data: "2025-06-01", valor: 150.00},
+    {transacao: "Salário", categoria: "Receita", data: "2025-06-05", valor: 3000.00},
+]
